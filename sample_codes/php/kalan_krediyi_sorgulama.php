dca7f96348eb5ccc2647fd42fd2be8ac7131f31b
vasifhuseynov <31925263+vasifhuseynov@users.noreply.github.com>
2017-10-05 09:39:03 +0300

    Create kalan_krediyi_sorgulama.php

---
+++ sample_codes/php/kalan_krediyi_sorgulama.php
@@ -0,0 +1,23 @@
+<?php
+//SMS API ile kalan krediyi sorgulama örneği (PHP)
+//Aşağıdaki örnek kodu kendinize özelleştirerek kullanabilirsiniz.
+
+function queryCredits(){
+  $username="xxxx";
+  $password=urlencode("xxxx");
+  $url= "http://sms.verimor.com.tr/v2/balance?username=$username&password=$password";
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  $http_response = curl_exec($ch);
+  $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  if($http_code != 200){
+    echo "$http_code $http_response\n";
+    return false;
+  }
+ 
+  $balance = $http_response; // 4532
+  echo "Mevcut krediniz: $balance";
+  return $balance;
+}
+ 
+queryCredits();
